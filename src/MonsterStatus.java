2c47fa65f00b03fccdc91ad8eb37f6054ebdff00
ssk-1213 <sskasm1213@gmail.com>
2026-09-02 09:53:51 +0900

    Refactor MonsterStatus constructor for better readability

--- src/MonsterStatus.java
+++ src/MonsterStatus.java
@@ -6,9 +6,7 @@ public class MonsterStatus {
     int atk;
     int def;
 
-    public MonsterStatus(String name, int hp, int maxHp,
-                         allElement.Element element,
-                         int atk, int def) {
+    public MonsterStatus(String name, int hp, int maxHp, allElement.Element element, int atk, int def) {
         this.name = name;
         this.hp = hp;
         this.maxHp = maxHp;
